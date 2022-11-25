86e818cfd019223112fc294d704615d8792e1ac7
root <root@Leo>
2022-11-25 15:54:14 +0100

    Project functional

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int n = 0;
+    float sum=0;
+    printf("Veuillez saisir le nombre d'élèves \n");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        int note = -1;
+        while((note<0) || (note >20)){
+            printf("Veuillez saisir la note de l'élève numéro %d \n", (i+1));
+            scanf("%d", &note);
+            if((note>0) && (note<20)){
+                if(note>=10){
+                    printf("L'élève numéro %d est admis \n", (i+1));
+                }else{
+                    printf("L'élève numéro %d n'est pas admis \n", (i+1));
+                }
+            }
+        }
+        sum += note;
+    }
+    sum = sum / n;
+    printf("la moyenne de la classe est %f /20", sum);
 }
